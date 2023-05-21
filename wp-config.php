b25079118a155da1052f9a6e00a3d0614980d1fc
Anthony Mastoroudes <anthony.mast@gmail.com>
2023-05-21 11:58:32 +1000

    Removed error log

--- wp-config.php
+++ wp-config.php
@@ -1,12 +1,9 @@
 <?php
 // WP_REDIS MUST BE SET FIRST!!!
 if (getenv('USE_WP_REDIS') ?: false) {
-	error_log("USE_WP_REDIS=true");
 	define("WP_REDIS_PORT",  getenv('WP_REDIS_PORT') ?: '6379');
 	define("WP_REDIS_HOST", getenv('WP_REDIS_HOST') ?: 'localhost');
 	define("WP_REDIS_PASSWORD", getenv('WP_REDIS_PASSWORD') ?: 'password_here');
-} else {
-	error_log("USE_WP_REDIS=false");
 }
 /**
  * The base configuration for WordPress
@@ -30,7 +27,6 @@ if (getenv('USE_WP_REDIS') ?: false) {
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', getenv('DB_NAME') ?: 'database_name_here');
-error_log("DB_NAME=" . DB_NAME);
 
 /** Database username */
 define('DB_USER', getenv('DB_USER') ?: 'username_here');
@@ -40,7 +36,6 @@ define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'password_here');
 
 /** Database hostname */
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-error_log("DB_HOST=" . DB_HOST);
 
 /** Database charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -92,12 +87,9 @@ $table_prefix = getenv('TABLE_PREFIX') ?: 'wp_';
 define('WP_DEBUG', getenv('WP_DEBUG') ?: false); // Or false
 
 if (WP_DEBUG) {
-	error_log("WP_DEBUG=true");
 	define('WP_DEBUG_LOG', true);
 	define('WP_DEBUG_DISPLAY', false);
 	@ini_set('display_errors', 0);
-} else {
-	error_log("WP_DEBUG=false");
 }
 
 /* Add any custom values between this line and the "stop editing" line. */
@@ -120,9 +112,6 @@ define('EMPTY_TRASH_DAYS', getenv('EMPTY_TRASH_DAYS') ?: 30); // 30 days
 /** Enable WordPress Cache. */
 if (getenv('WP_CACHE') ?: false) {
 	define('WP_CACHE', getenv('WP_CACHE') ?: true);
-	error_log("WP_CACHE=true");
-} else {
-	error_log("WP_CACHE=false");
 }
 
 /** Maximum Memory Limit. */
@@ -132,24 +121,18 @@ define('WP_MAX_MEMORY_LIMIT', getenv('WP_MAX_MEMORY_LIMIT') ?: '128M');
 define('WP_MEMORY_LIMIT', getenv('WP_MEMORY_LIMIT') ?: '96M');
 
 if (getenv('USE_FORWARD_PROXY') ?: false) {
-	error_log("USE_FORWARD_PROXY=true");
 	// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
 	// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 	if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 		$_SERVER['HTTPS'] = 'on';
 	}
-} else {
-	error_log("USE_FORWARD_PROXY=false");
 }
 
 define('WP_HOME', getenv('WP_HOME') ?: 'https://' . $_SERVER['HTTP_HOST']);
 define('WP_SITEURL', getenv('WP_SITEURL') ?: 'https://' . $_SERVER['HTTP_HOST']);
 
 if (getenv("USE_AWS_CDN") ?: false) {
-	error_log("USE_AWS_CDN=true");
 	define('AWS_CDN_Isactive', getenv('AWS_CDN_Isactive') ?: '');
-} else {
-	error_log("USE_AWS_CDN=false");
 }
 
 /* That's all, stop editing! Happy publishing. */
